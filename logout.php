aa79ec3aea9e83bdd25ee6f6c1f3d635accf67d0
Sara Bednaić <77974879+sarabednaic@users.noreply.github.com>
2025-05-25 21:15:41 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,12 @@
 <?php
+//pokretanje sesije
 session_start();
+//uništavanje sesije
 session_destroy();
+
+//preusmjeravanje korisnika na početnu stranicu
 header("Location: index.php");
+
+//izlaz iz koda
+exit();
 ?>
